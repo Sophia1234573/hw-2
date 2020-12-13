dc240028008fcac6ca5a78a09ac381a5a548e144
sophia <sophiadenisovichgovtva@gmail.com>
2020-12-13 15:58:10 +0200

    added file

---
+++ NameEncoderDecoder.java
@@ -0,0 +1,26 @@
+public class NameEncoderDecoder {
+    public String encode(String name) {
+        String newName = name
+                   .replace("e","1")
+                   .replace("u","2")
+                   .replace("i","3")
+                   .replace("o","4")
+                   .replace("a","5");
+        return "NOTFORYOU" + newName + "YESNOTFORYOU";
+    }
+    public String decode(String name) {
+        return name.replace("1","e")
+                   .replace("2","u")
+                   .replace("3","i")
+                   .replace("4","o")
+                   .replace("5","a")
+                   .replace("NOTFORYOU","")
+                   .replace("YES","");                   
+    } 
+    public static void main(String[] args) {
+        NameEncoderDecoder testName = new NameEncoderDecoder();
+        System.out.println("Crab - " + testName.encode("Crab"));
+        System.out.println("NOTFORYOUCr5bYESNOTFORYOU - " + testName.decode("NOTFORYOUCr5bYESNOTFORYOU"));
+        System.out.println("NOTFORYOUNOTFORYOUYESNOTFORYOU - " + testName.decode("NOTFORYOUNOTFORYOUYESNOTFORYOU"));        
+    }
+}
